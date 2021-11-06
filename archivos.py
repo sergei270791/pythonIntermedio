e959bb027e6d0469326eb1b18b4d715c69e4d764
sergei270791 <sergei.calle.c@uni.pe>
2021-11-06 17:59:16 -0500

    manejo de archivos.py

---
+++ archivos.py
@@ -0,0 +1,25 @@
+#-*- coding UTF-8 -*-
+
+
+def read():
+    numbers = []
+    with open("./archivos/numbers.txt", "r",encoding='utf-8') as f: 
+        for line in f:
+            numbers.append(int(line))
+    print(numbers)
+
+
+def write():
+    names = ["Facundo", "Gregorio", "Marta", "Susana", "José"]
+    with open("./archivos/names.txt", "w",encoding='utf-8') as f:
+        for name in names: 
+            f.write(name)
+            f.write("\n")
+
+
+def run():
+    write()
+
+
+if __name__ == '__main__':
+    run()
