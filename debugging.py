4f5345ec9e64c0d2cd0844394c2051785f1c776e
sergei270791 <sergei.calle.c@uni.pe>
2021-11-06 02:05:43 -0500

    debugging

---
+++ debugging.py
@@ -0,0 +1,19 @@
+#-*- coding UTF-8 -*-
+
+
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+
+def run():
+    num = int(input('Ingresa un número: '))
+    print(divisors(num))
+    print("Terminó mi programa")
+
+
+if __name__ == '__main__':
+    run()
