2a8519b6dbacb83216a4fd3684c3984ebaba2c45
sergei270791 <sergei.calle.c@uni.pe>
2021-11-07 03:50:31 -0500

    juego con manejo de errores

--- juego_ahorcado.py
+++ juego_ahorcado.py
@@ -13,14 +13,24 @@ def read_data():
 def run():
     palabra=read_data()
     mostrar=list('_'*len(palabra))
+    os.system('clear')
     while '_' in mostrar:
-        os.system('clear')
         print('Adivina la palabra: ')
         print("".join(mostrar))
-        letra=input('Ingrese una letra: ')
-        for i,char in enumerate(list(palabra)):
-            if char==letra:
-                mostrar[i]=char.upper()
+        try:
+            letra=input('Ingrese una letra: ')
+            if not letra.isalpha():
+                raise ValueError('Solo puede ingresar letras, intenta denuevo')
+            for i,char in enumerate(list(palabra)):
+                letra=letra.upper()
+                char=char.upper()
+                if char==letra:
+                    mostrar[i]=char.upper()
+            os.system('clear')
+        except ValueError as ve:
+            os.system('clear')
+            print('Error:', ve)
+            continue
     os.system('clear')
     print('¡Ganaste!, La palabra era '+palabra.upper())
 
